456c3b619363f7b02a500dee28cec54bc26187bc
nonsens <nonsens1212@gmail.com>
2026-05-12 21:33:09 +0400

    Добавьте файлы проекта.

---
+++ SignsCreatorWeb.Application/Interfaces/IKompasService.cs
@@ -0,0 +1,13 @@
+﻿using Kompas6API5;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignsCreatorWeb.Application.Interfaces
+{
+    public interface IKompasService
+    {
+
+        public bool OpenKompas();
+    }
+}
---
+++ SignsCreatorWeb/Controllers/KompasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignsCreatorWeb.Application.Interfaces;
+
+namespace SignsCreatorWeb.Controllers
+{
+    //[ApiController]
+    //[Route("[controller]")]
+    public class KompasController: ControllerBase
+    {
+        private readonly IKompasService _kompasService;
+        public KompasController(IKompasService kompasService)
+        {
+            _kompasService=kompasService;
+            
+        }
+        [HttpPost("OpenKompas")]
+        public async Task<ActionResult> OpenKompas()
+        {
+            bool startKompas = _kompasService.OpenKompas();
+
+            return Ok();
+        }
+    }
+}
